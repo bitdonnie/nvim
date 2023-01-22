e907b37d611f8f0f09e0ed8a3c1beb02bb5a6388
donvdkrogt <don.k@me.com>
2023-01-22 22:10:16 +0100

    Add `.lua` files

---
+++ lua/opts.lua
@@ -0,0 +1,43 @@
+local opt = vim.opt -- with vim.opt we can set global, window and buffer settings, acting like :set in vimscript
+local set = vim.opt
+local o = vim.o
+local cmd = vim.api.nvim_command
+
+opt.number = true -- bool: Show line numbers
+opt.relativenumber = true -- bool: Show relative line numbers
+opt.scrolloff = 4 -- int:  Min num lines of context
+opt.signcolumn = "yes" -- str:  Show the sign column
+opt.numberwidth = 4 -- gutter width
+set.cursorline = true -- display cursor line
+set.cursorlineopt = 'number' 
+
+vim.keymap.set('n', '<C-k>', '<Esc>')
+vim.keymap.set('i', '<C-k>', '<Esc>')
+
+-- [[ Theme ]]
+opt.syntax = "ON" -- str:  Allow syntax highlighting
+opt.termguicolors = true -- bool: If term supports ui color then enable
+
+
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+opt.shortmess = vim.opt.shortmess + { c = true}
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- Treesitter folding 
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
---
+++ lua/plugins.lua
@@ -0,0 +1,35 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+        
+    use 'williamboman/mason.nvim'    
+    use 'williamboman/mason-lspconfig.nvim'
+
+    use 'neovim/nvim-lspconfig' 
+    use 'simrat39/rust-tools.nvim'
+
+    -- Completion framework:
+    use 'hrsh7th/nvim-cmp' 
+
+    -- LSP completion source:
+    use 'hrsh7th/cmp-nvim-lsp'
+
+    -- Useful completion sources:
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp-signature-help'
+    use 'hrsh7th/cmp-vsnip'                             
+    use 'hrsh7th/cmp-path'                              
+    use 'hrsh7th/cmp-buffer'                            
+    use 'hrsh7th/vim-vsnip'
+    
+    -- color theme
+    use "rebelot/kanagawa.nvim"
+
+    -- Tree setter
+    use 'nvim-treesitter/nvim-treesitter'
+
+    -- Debugging
+    use 'mfussenegger/nvim-dap'
+
+end)
